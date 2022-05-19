d928682c89155d402f7223e33ac362ba6d98d6d7
Teanna Taylor <teannamtaylor95@gmail.com>
2022-05-19 10:35:21 -0700

    commit tree b2eaa1442fcaa66651563a9ad2d9a4198c142c79

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+
+let random = Math.floor(Math.random()*10) + 1;
+
+let num3 = 8;
+let num4 = 2;
+
+const mod = num3 / num2 
+
+let max = Math.max(1,5,6,7,20);
+
